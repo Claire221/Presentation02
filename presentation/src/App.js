24ca26958ad3eb6c007b8706aae0f2c86199b8ac
Claire221 <c.biglands@hotmail.com>
2022-12-15 09:05:30 +0000

    Add the project setup

---
+++ presentation/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import MyGallery from "./pages/Gallery"
+
+function App() {
+  return (
+    <div className="App">
+      <MyGallery/>
+    </div>
+  );
+}
+
+export default App;
